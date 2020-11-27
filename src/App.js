7b2f5d2052b2b593afd3c03375d845bb51acbded
alimtegar <alimtegar4@gmail.com>
2020-11-28 00:06:31 +0700

    Create function for rendering prediction box

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useEffect, useRef } from 'react';
+import * as faceapi from 'face-api.js';
 
 function App() {
+  const WIDTH = 360;
+  const HEIGHT = 480;
+  const FACE_API_MODELS_URI = '/models/face-api-models';
+  const videoRef = useRef();
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    const webCamPromise = navigator.mediaDevices
+      .getUserMedia({ video: true, })
+      .then((stream) => {
+        window.stream = stream;
+        videoRef.current.srcObject = stream;
+
+        return new Promise((resolve, _) => {
+          videoRef.current.onloadedmetadata = () => {
+            resolve();
+          };
+        });
+      });
+
+    const faceapiPromise = faceapi.nets.ssdMobilenetv1.loadFromUri(FACE_API_MODELS_URI);
+
+    Promise.all([
+      webCamPromise,
+      faceapiPromise,
+    ])
+      .then(() => {
+        detectFrame(videoRef.current)
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const detectFrame = (input) => {
+    faceapi.detectAllFaces(input)
+      .then((faceDetections) => {
+        renderDetectionBox(faceDetections);
+
+        setTimeout(() => { detectFrame(videoRef.current); }, 1000);
+      });
+  };
+
+  const renderDetectionBox = (faceDetections) => {
+    const ctx = canvasRef.current.getContext('2d');
+
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    faceDetections.map((faceDetection) => {
+      if (faceDetection) {
+        // console.log(faceDetection);
+        const x = faceDetection.box._x;
+        const y = faceDetection.box._y;
+        const w = faceDetection.box._width;
+        const h = faceDetection.box._height;
+
+        // Draw the bounding box
+        ctx.strokeStyle = "#FF0000";
+        ctx.lineWidth = 4;
+        ctx.strokeRect(x, y, w, h);
+      }
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <video
+        autoPlay
+        playsInline
+        muted
+        width={WIDTH}
+        height={HEIGHT}
+        ref={videoRef}
+      />
+      <canvas
+        width={WIDTH}
+        height={HEIGHT}
+        ref={canvasRef}
+      />
     </div>
   );
 }
